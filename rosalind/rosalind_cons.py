4e44e2380e073271288361e1b2896f5d7c210212
victorhlopezd <122932510+victorhlopezd@users.noreply.github.com>
2023-08-01 10:29:09 -0700

    Update rosalind_cons.py

--- rosalind/rosalind_cons.py
+++ rosalind/rosalind_cons.py
@@ -2,7 +2,7 @@
 
 import sys
 
-#Finding a Most Likely Common Ancestor
+#Consensus and Profile
 
 #Given: A collection of at most 10 DNA strings of equal length (at most 1 kbp) in FASTA format. 
 #Return: A consensus string and profile matrix for the collection. (If several possible consensus strings exist, then you may return any one of them.)
